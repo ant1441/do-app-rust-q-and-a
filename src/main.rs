560ce0e6bface1117b56f49efdb826fbb23de79c
Adam <ant1441@gmail.com>
2025-03-13 21:21:23 +0000

    feat: Add dynamic path route

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,12 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/hello/<name>")]
+fn hello(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, hello])
 }
