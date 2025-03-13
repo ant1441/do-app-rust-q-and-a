086a98208caf4c223b7d6836fdc0b00eaa9f96c8
Adam <ant1441@gmail.com>
2025-03-13 22:58:23 +0000

    feat: DB support

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,67 @@
-use rocket_prometheus::PrometheusMetrics;
-
 #[macro_use]
 extern crate rocket;
+
+use std::{env, net::SocketAddr};
+
+use rocket::{Request, fairing::AdHoc};
+use rocket_db_pools::{Database, sqlx};
+use rocket_prometheus::PrometheusMetrics;
+
+mod db;
+use db::Db;
+
+#[derive(Debug)]
+
+pub struct RequestSocketAddr {
+    pub socket_addr: SocketAddr,
+}
+
+use rocket::request::{self, FromRequest};
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for RequestSocketAddr {
+    type Error = ();
+
+    async fn from_request(req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
+        let socket_addr = req.remote().unwrap();
+        request::Outcome::Success(Self { socket_addr })
+    }
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/hello/<name>")]
-fn hello(name: &str) -> String {
-    format!("Hello, {}!", name)
+async fn run_migrations(rocket: rocket::Rocket<rocket::Build>) -> rocket::fairing::Result {
+    match Db::fetch(&rocket) {
+        Some(db) => match sqlx::migrate!().run(&**db).await {
+            Ok(_) => Ok(rocket),
+            Err(e) => {
+                error!("Failed to initialize SQLx database: {}", e);
+                Err(rocket)
+            }
+        },
+        None => Err(rocket),
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
+    let database_url = if cfg!(debug_assertions) {
+        "".to_string()
+    } else {
+        env::var("DATABASE_URL").expect("Missing DATABASE_URL")
+    };
+
+    let figment = rocket::Config::figment().merge(("databases.q_and_a.url", database_url));
+
     let prometheus = PrometheusMetrics::new();
-    rocket::build()
+
+    rocket::custom(figment)
         .attach(prometheus.clone())
-        .mount("/", routes![index, hello])
+        .attach(Db::init())
+        .attach(AdHoc::try_on_ignite("SQLx Migrations", run_migrations))
+        .mount("/", routes![index])
         .mount("/metrics", prometheus)
 }
